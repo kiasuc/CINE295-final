056a9dd0eeef1d6bfda88592fcf23dab9dcd67a1
ethan <ethanchen246@gmail.com>
2026-05-12 12:29:52 -0700

    Pages setup

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import { defineConfig } from "vite";
 export default defineConfig({
   root: ".",
   server: { port: 5180, strictPort: true, open: false },
+  base: "/CINE295-final/",
   build: { outDir: "dist", emptyOutDir: true },
 });
